6c851332800fc6db5474a8758e6eeeef512ab472
YOUR NAME <YOUR EMAIL>
2026-02-21 14:39:49 +0700

    Fix login request object

--- database.py
+++ database.py
@@ -1,13 +1,19 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./nutrition.db"
+DATABASE_URL = os.getenv("postgresql://nutrition_user:n4h0O5qx27fnovgyIjfxsDQl1RNKCQt1@dpg-d6clvr7fte5s73cs5s3g-a/nutrition_p23w")
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
+
