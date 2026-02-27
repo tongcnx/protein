dcb5d4415166c3c71a9203d58b6899e7dc6090a8
YOUR NAME <YOUR EMAIL>
2026-02-27 15:07:03 +0700

    fix imports

--- app/database/database.py
+++ app/database/database.py
@@ -1,9 +1,9 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = os.getenv("postgresql://nutrition_user:84Zv89By90MpWjKv1hfXnjpXH5rT18Jo@dpg-d6dbfoktgctc73f1n400-a.singapore-postgres.render.com/nutrition_1t51")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
