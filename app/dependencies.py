0c814d1caf6b68a4b31461f092e2ac78e94894d7
YOUR NAME <YOUR EMAIL>
2026-03-04 11:52:17 +0700

    fix imports

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,39 +1,30 @@
 from fastapi import Depends, HTTPException
-from jose import jwt, JWTError
+from jose import jwt
 from sqlalchemy.orm import Session
 
 from app.database.database import SessionLocal
 from app.models.user import User
 
-SECRET_KEY="SUPER_SECRET_KEY_12345"
-ALGORITHM="HS256"
+SECRET_KEY = "MYSECRET123"
+ALGORITHM = "HS256"
 
 
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
 
-def get_current_user(
-    token:str,
-    db:Session=Depends(get_db)
-):
+def get_current_user(token: str, db: Session = Depends(get_db)):
 
     try:
-        payload=jwt.decode(
-            token,
-            SECRET_KEY,
-            algorithms=[ALGORITHM]
-        )
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload["user_id"]
+    except:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
-        user_id=payload.get("user_id")
-
-    except JWTError:
-        raise HTTPException(401,"invalid token")
-
-    user=db.query(User).get(user_id)
+    user = db.query(User).filter(User.id == user_id).first()
 
     return user
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+
 from app.database.database import SessionLocal
-from app.database.database import Base
 from app.models.user import User
 
-router = APIRouter()
+from jose import jwt
+from datetime import datetime, timedelta
+
+router = APIRouter(prefix="/auth")
+
+SECRET_KEY = "MYSECRET123"
+ALGORITHM = "HS256"
+
 
 def get_db():
     db = SessionLocal()
@@ -14,7 +21,7 @@ def get_db():
         db.close()
 
 
-@router.post("/auth/register")
+@router.post("/register")
 def register(name: str, email: str, password: str, db: Session = Depends(get_db)):
 
     user = User(
@@ -25,6 +32,30 @@ def register(name: str, email: str, password: str, db: Session = Depends(get_db)
 
     db.add(user)
     db.commit()
-    db.refresh(user)
 
     return {"status": "created"}
+
+
+@router.post("/login")
+def login(email: str, password: str, db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if user.password != password:
+        raise HTTPException(status_code=401, detail="Wrong password")
+
+    token = jwt.encode(
+        {
+            "user_id": user.id,
+            "exp": datetime.utcnow() + timedelta(days=7)
+        },
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return {
+        "token": token
+    }
